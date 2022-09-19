02925f03856be9a2f228795ed0b5db646caa7844
Duck1s <32653@ma-web.nl>
2022-09-19 11:36:20 +0200

    feat(header): add header content with background image

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
 // Imports
+import "./Header.css"
 
 // Functions
 const Header = () => {
 	return (
 		<>
-			<h2>Ik ben header</h2>
+			<header className='header'>
+				<h1>Grote titel</h1>
+				<h2>Kleine titel</h2>
+				<button>Bestel</button>
+			</header>
 		</>
 	)
 }
