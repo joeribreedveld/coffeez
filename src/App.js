b3ed21a8993de3b14cfc23209ee790c4614dcf10
Duck1s <32653@ma-web.nl>
2022-09-19 11:29:30 +0200

    feat(header): make basic header component without content yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // Imports
 import "./App.css"
 import Navigation from "./Navigation"
+import Header from "./Header"
 
 // Functions
 const App = () => {
 	return (
 		<>
 			<Navigation />
+			<Header />
 		</>
 	)
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+// Imports
+
+// Functions
+const Header = () => {
+	return (
+		<>
+			<h2>Ik ben header</h2>
+		</>
+	)
+}
+
+// Exports
+export default Header
