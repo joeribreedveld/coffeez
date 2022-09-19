5ed398c3bf67001b9c79bbc895428bdfd7762c5d
Duck1s <32653@ma-web.nl>
2022-09-19 18:23:32 +0200

    feat(pages): make homepage with all the content

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 // Imports
 import "./App.css"
-import Navigation from "./components/Navigation/Navigation"
-import Header from "./components/Header/Header"
-import Card from "./components/Card/Card"
 import "./Section/Section.css"
-import Section from "./Section/Section"
+import HomePage from "./pages/Homepage"
 
 // Functions
 const App = () => {
 	return (
 		<>
-			<Navigation />
-			<Header bigTitle='Coffeez' smallTitle='Coffee that will make you smile' buttonText='order now' />
-			<Section title='Het is mooi weer vandaag'>
-				<Card text='Koffie is lekker' />
-				<Card image='coffee3.jpg' text='Koffie is vies' />
-				<Card image='coffee4.jpg' text='Koffie is sappig' />
-				<Card image='coffee5.jpg' text='Hoiiii' />
-			</Section>
-			<Section title='Koekjes zijn lekker'>
-				<Card text='Hoi' />
-				<Card image='coffee.jpg' text='Koekjes zijn lekker' />
-				<Card image='coffee5.jpg' text='Koffie is sappig' />
-				<Card image='coffee4.jpg' text='Koekjes zijn lekker' />
-			</Section>
+			<HomePage />
 		</>
 	)
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,30 @@
+// Imports
+import Navigation from "../components/Navigation/Navigation"
+import Header from "../components/Header/Header"
+import Card from "../components/Card/Card"
+import "../Section/Section.css"
+import Section from "../Section/Section"
+
+// Functions
+const HomePage = () => {
+	return (
+		<>
+			<Navigation />
+			<Header bigTitle='Coffeez' smallTitle='Coffee that will make you smile' buttonText='order now' />
+			<Section title='Het is mooi weer vandaag'>
+				<Card text='Koffie is lekker' />
+				<Card image='coffee3.jpg' text='Koffie is vies' />
+				<Card image='coffee4.jpg' text='Koffie is sappig' />
+				<Card image='coffee5.jpg' text='Hoiiii' />
+			</Section>
+			<Section title='Koekjes zijn lekker'>
+				<Card text='Hoi' />
+				<Card image='coffee.jpg' text='Koekjes zijn lekker' />
+				<Card image='coffee5.jpg' text='Koffie is sappig' />
+				<Card image='coffee4.jpg' text='Koekjes zijn lekker' />
+			</Section>
+		</>
+	)
+}
+
+export default HomePage
